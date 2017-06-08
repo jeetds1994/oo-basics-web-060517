4c5c815548a4db0c9f73000acb0d80b4f0ce200b
Diljeet <jeetds1994@gmail.com>
2017-06-08 14:49:28 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,44 @@
 
+class Book
 
+  def initialize(input)
+    @title = input
+  end
+
+  def title
+    @title
+
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(input)
+    @page_count = input
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(input)
+    @genre = input
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+
+end
+
+
+#learn spec/01_book_spec.rb
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,58 @@
 # Make your shoe class here!
+
+
+class Shoe
+
+  def initialize(input)
+    @brand = input
+  end
+
+  def brand=(input)
+    @brand = input
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(input)
+    @color = input
+  end
+
+  def color
+    @color
+  end
+
+  def size=(input)
+    @shoe_size = input
+  end
+
+  def size
+    @shoe_size
+  end
+
+  def material=(input)
+    @mat = input
+  end
+
+  def material
+    @mat
+  end
+
+  def condition=(input)
+    @cond = input
+  end
+
+  def condition
+    @cond
+  end
+
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @cond = "new"
+  end
+end
+
+
+#learn spec/01_book_spec.rb
